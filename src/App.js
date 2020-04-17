eb78884c4d6df9cb0756708d0263665752cdd9f9
Aren <aren.dalloul@colorado.edu>
2020-04-17 11:44:49 -0600

    change to allow server to connect to spotify

--- src/App.js
+++ src/App.js
@@ -147,6 +147,7 @@ class App extends Component {
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
+    console.log(this.state);
     return (
       <div className="App">
       <Particles className='particles'
