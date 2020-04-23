d7e14321be7fcc0f59b56f719ecc01fc43395dc9
Matthew Strong <mast4878@colorado.edu>
2020-04-23 17:40:49 -0600

    basic features working

--- mamba-magenta/mamba_magenta/const.py
+++ mamba-magenta/mamba_magenta/const.py
@@ -1,6 +1,10 @@
+import os
 VERSION = 1
 # artists - 7 models
 ARTISTS = ['MelodyRNN', 'PerformanceRNN', 'PolyphonyRNN',
            'PianoRollRNNNade', 'ImprovRNN', 'MusicVAE', 'MusicTransformer']
 
 SPOTIFY_SEARCH_ENDPOINT = 'https://api.spotify.com/v1/search'
+SPOTIFY_TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
+CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
--- mamba-magenta/mamba_magenta/lakh.py
+++ mamba-magenta/mamba_magenta/lakh.py
@@ -8,23 +8,24 @@ import const as C
 # use spotify api to identify artists
 
 
-def get_cleaned_midis():
+def get_spotify_bearer_token():
     """
-    gets cleaned midis of files.
+    gets the spotify token.
+    The client id and secrets are defined in `const.py`
+    and just get the corresponding key from `os.environ`.
     """
-    tarname = 'clean_midi.tar.gz'
-    if os.path.isdir(os.path.join(os.getcwd(), 'data/clean_midi')):
-        print('MIDI data already exists! Skipping download...')
-    else:
-        url = 'http://hog.ee.columbia.edu/craffel/lmd/clean_midi.tar.gz'
-        # requests.get(url)
-        # using wget will show progress of download instead of seeming to hang.
-        os.system(f'wget {url}')
-        shutil.move(tarname, 'data')
-        my_tar = tarfile.open(f'data/{tarname}')
-        my_tar.extractall('data/')
-        my_tar.close()
-        os.remove('data/{tar}')
+    client_id = C.CLIENT_ID
+    client_secret = C.CLIENT_SECRET
+    grant_type = 'client_credentials'
+    body_params = {'grant_type': grant_type}
+    get_token_url = C.SPOTIFY_TOKEN_ENDPOINT
+    response = requests.post(get_token_url,
+                             data=body_params,
+                             auth=(client_id, client_secret))
+
+    token_raw = json.loads(response.text)
+    token = token_raw["access_token"]
+    return token
 
 
 def get_genres(bearer_auth, artist_name='Beethoven'):
@@ -35,14 +36,65 @@ def get_genres(bearer_auth, artist_name='Beethoven'):
 
     params = {'q': artist_name, 'type': 'artist'}
     data = requests.get(api_link, params=params, auth=bearer_auth).json()
-    top_entry = data['artists']['items'][0]
-    # for future reference, top_entry['name'] is the name of the top result
-    return top_entry['genres']
+    items = data['artists']['items']
+    if len(items) != 0:
+        top_entry = data['artists']['items'][0]
+        # for future reference, top_entry['name'] is the name of the top result
+        name = top_entry['name']
+        # print(f'Artist: {name} vs {artist_name}')
+        return top_entry['genres']
+    else:
+        return []
 
 
-def get_genres_from_folders():
-    os.listdir("data/")
-    pass
+class LokhDataset():
+    def __init__(self):
+        self.main_dir = 'data/clean_midi'
+        self.get_cleaned_midis()
+
+    def get_cleaned_midis(self):
+        """
+        gets cleaned midis of files.
+        """
+        tarname = 'clean_midi.tar.gz'
+        if os.path.isdir(os.path.join(os.getcwd(), self.main_dir)):
+            print('MIDI data already exists! Skipping download...')
+
+        else:
+            url = 'http://hog.ee.columbia.edu/craffel/lmd/clean_midi.tar.gz'
+
+            # using wget will show progress of download instead of seeming to hang.
+            os.system(f'wget {url}')
+            shutil.move(tarname, 'data')
+            my_tar = tarfile.open(self.main_dir)
+            my_tar.extractall('data/')
+            my_tar.close()
+            os.remove('data/{tarname}')
+
+    def get_genres_from_folders(self, dir_name='clean_midi'):
+        d = {}
+        artist_names = os.listdir(self.main_dir)
+        for name in artist_names:
+            songs_dir = os.path.join(self.main_dir, name)
+            if os.path.isdir(songs_dir):
+                num_songs = len(os.listdir(os.path.join(self.main_dir, name)))
+                genres = get_genres(bearer, artist_name=name)
+                if len(genres) > 0:
+                    main_genre = None
+                    filled = False
+
+                    for genre in genres:
+                        base_genre = genre.split()[-1]
+                        if main_genre is None:
+                            main_genre = base_genre
+
+                        if base_genre in d.keys():
+                            d[base_genre] = d.get(base_genre, 0) + 1
+                            filled = True
+                            break
+
+                    if not filled:
+                        d[main_genre] = 1
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -58,7 +110,9 @@ class BearerAuth(requests.auth.AuthBase):
 
 
 if __name__ == '__main__':
-    token = os.environ['SPOTIFY_API_TOKEN']
+    token = get_spotify_bearer_token()
     bearer = BearerAuth(token)
     genres = get_genres(bearer)
     print(genres)
+    data = LokhDataset()
+    data.get_genres_from_folders()
